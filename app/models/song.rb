0db83483617e41e26e3e7ed054bb132ff016e392
github username <crishanks12@gmail.com>
2019-02-25 20:47:18 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  before_save :make_title_case
+
+  def make_title_case
+    self.title.titlecase
+  end
+
+  def artist_name
+    self.artist.name
+  end
 end
